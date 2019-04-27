ff54cc99e1b316b5b076e509a72d9e25356ceb62
Admin <Admin@Aparnas-MacBook-Pro-3.local>
2019-04-27 12:29:11 +0530

    testng added

--- src/test/java/gmailLoginTestPackage/LandingPageTest.java
+++ src/test/java/gmailLoginTestPackage/LandingPageTest.java
@@ -1,5 +1,31 @@
 package gmailLoginTestPackage;
 
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
 public class LandingPageTest {
+	
+	@Test
+	public void test1() {
+		System.out.print("test1");
+	}
+	
+	@Test
+	public void test2() {
+		System.out.print("test2");
+	}
+	
+	@Test
+	public void test3() {
+		System.out.print("test3");
+	}
+	
+	@Test
+	public void test4() {
+		System.out.print("test4");
+	}
+	
+	
+	
 
 }
--- src/test/java/gmailLoginTestPackage/LoginTest.java
+++ src/test/java/gmailLoginTestPackage/LoginTest.java
@@ -1,5 +1,14 @@
 package gmailLoginTestPackage;
 
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
 public class LoginTest {
+	
+	@Test
+	public void logInTest() {
+		
+		
+	}
 
 }
